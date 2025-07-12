2b3db9380bbd607435620f6abd291c971efc176f
Hp <ahmedelzanaty001@gmail.com>
2025-07-13 02:49:55 +0300

    init commit

---
+++ src/person.java
@@ -0,0 +1,10 @@
+public class person {
+    String name ;
+    int age ;
+
+    public person(String name , int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("name is "+name +" my age is "+age);
+    }
+}
